e4e8a580313550955ed05e25cd80563537039b1a
RSwebdevelop <ranjan.webdeveloper@gmail.com>
2023-02-11 12:14:09 +0530

    first commit

---
+++ src/myCompnents/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+
+export default class Footer extends Component () {
+    render(){
+        return (
+            <div>Footer
+                <h3>I am  the Footer component</h3>
+            </div>
+          )
+    }
+ 
+}
---
+++ src/myCompnents/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Header
+    <h1>I am the Header Compmenent</h1>
+    </div>
+  )
+}
+
+export default Header
